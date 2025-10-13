cabfb5674fac4880a57d13e6a12dc4f561891d58
pritam <wepritam.18@gmail.com>
2025-10-13 13:27:10 +0530

    feat: add vanilla-tilt dependency and update Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["vanilla-tilt"],
     },
   },
 });
