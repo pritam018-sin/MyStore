f086ce848e912c78d2c4d45246fed0c59d487533
pritam <wepritam.18@gmail.com>
2025-10-13 13:32:53 +0530

    fix: correct import path for cartUtils in cartSlice

--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { updateCart } from "../../../Utils/cartUtils";
+import { updateCart } from "../../../utils/cartUtils.js";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
